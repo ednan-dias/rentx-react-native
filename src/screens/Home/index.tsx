80bf878a50c89affa5496a340855515659acb95a
Ednan Dias <ednan.741@gmail.com>
2021-11-15 15:52:30 -0300

    tela de detalhes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,9 +3,22 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
-import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { Car } from "../../components/Car";
+
+import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
 
 export function Home() {
+  const carData = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: "Ao dia",
+      price: 120,
+    },
+    thumbnail:
+      "https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png",
+  };
+
   return (
     <Container>
       <StatusBar
@@ -19,6 +32,12 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <CarList
+        data={[1, 2, 3, 4, 5, 6, 7]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
     </Container>
   );
 }
